d08d83933eec95c84219e9c70f3060ce52cb100a
Filiomondria <f.oszczepalinski@gmail.com>
2023-03-13 12:04:50 +0100

    Update LISTA1.R

--- LISTA1.R
+++ LISTA1.R
@@ -2,9 +2,16 @@ library(vcdExtra)
 library(dplyr)
 library(ggplot2)
 library(ggmosaic)
-
-data <- read.csv2('C:\\Users\\Filip\\Desktop\\ANKIETYZACJA DANYCH\\personel.csv', header = TRUE)
+library(RColorBrewer)
+library(devtools)
+library(productplots)
+data <- read.csv2('C:\\Users\\Filip\\Desktop\\ANKIETYZACJA DANYCH\\personel.csv', header = FALSE)
 colnames(data) <- c('D','S','A1','A2','W1', 'W2', 'P', 'Wiek', 'Wyk')
+
+my_data <- data.frame(data)
+
+my_data$A1 <- factor(my_data$A1)
+my_data$S <- factor(my_data$S)
 #####################
 
 View(data)
@@ -23,9 +30,78 @@ data_female %>% group_by(A1) %>% count()
 
 table(data['A2'])
 
+##################### Data Frame dla poszczegółnych grup ############################
+
+data_female <- data %>% filter(P ==  'K')
+data_female <- data_female[,!names(data_female) %in% c("P")]
+
+data_male <- data %>% filter(P ==  'M')
+data_male <- data_male[,!names(data_male) %in% c("P")]
+
+data_supply <- data %>% filter(D ==  'Z')
+data_supply <- data_supply[,!names(data_supply) %in% c("D")]
+
+data_working <- data %>% filter(D ==  'P')
+data_working <- data_working[,!names(data_working) %in% c("D")]
+
+data_sale <- data %>% filter(D ==  'S')
+data_sale <- data_sale[,!names(data_sale) %in% c("D")]
+
+data_service <- data %>% filter(D ==  'O')
+data_service <- data_service[,!names(data_service) %in% c("D")]
+
+data_menager <- data %>% filter(S ==  1)
+data_menager <- data_menager[,!names(data_menager) %in% c("S")]
+
+data_no_menager <- data %>% filter(S ==  0)
+data_no_menager <- data_no_menager[,!names(data_no_menager) %in% c("S")]
+
+data_up_age25 <- data %>% filter(Wiek ==  1)
+data_up_age25 <- data_up_age25[,!names(data_up_age25) %in% c("Wiek")]
+
+data_up_age35 <- data %>% filter(Wiek ==  2)
+data_up_age35 <- data_up_age35[,!names(data_up_age35) %in% c("Wiek")]
+
+data_up_age50 <- data %>% filter(Wiek ==  3)
+data_up_age50 <- data_up_age50[,!names(data_up_age50) %in% c("Wiek")]
+
+data_over_age50 <- data %>% filter(Wiek ==  4)
+data_over_age50 <- data_up_age50[,!names(data_up_age50) %in% c("Wiek")]
+
+data_education_vocat <- data %>% filter(Wyk ==  1)
+data_education_vocat <- data_education_vocat[,!names(data_education_vocat) %in% c("Wyk")]
+
+data_education_seco <- data %>% filter(Wyk ==  2)
+data_education_seco <- data_education_seco[,!names(data_education_seco) %in% c("Wyk")]
+
+data_education_high <- data %>% filter(Wyk ==  3)
+data_education_high <- data_education_high[,!names(data_education_high) %in% c("Wyk")]
+
+
+
+
 
 
 ### Zad 2 ###
+
+##### Color #######
+
+man <- '#495BF5'
+
+woman <- '#F442E1'
+
+no <- '#03E30A'
+
+yes <- '#F11803'
+
+o <- '#F9E301'
+
+p <- '#7F560D'
+
+s <- '#9C24C6'
+
+z <- '#726E74'
+
 
 ### Tabela wilodzietna W1 i P ###########
 structable(W1 ~ P, data)
@@ -97,28 +173,73 @@ ggplot(df_W2,aes(x=Var1, y=Freq, fill = Var1)) +
 
 #### Zad 4 ############
 
-ggplot(data = data) +
-  geom_mosaic(aes(x = product(A1), fill = D)) +
-  theme_mosaic()
+##### wzór mozaika #####
+
+ggplot(data = my_data) +
+  geom_mosaic(aes(x = product(A1,D), fill = A1)) +
+  theme_mosaic() +
+  scale_fill_manual(values = rev(c(brewer.pal(n = 5, name = "RdBu"))),
+                    name='ocena',
+                    labels = rev(c('zdecydowanie się zgadzam','zgadzam się',
+                                   'trudno powiedzieć','nie zgadzam się', 'zdecydowanie się nie zgadzam'))
+                    ) +
+  scale_x_productlist(labels = c("Obsługi","Produkcyjni","Sprzedaży","Zaopatrzenia"))+ 
+  theme(axis.text.y=element_blank(),axis.ticks.y=element_blank(), text = element_text(size = 15),axis.text.x = element_text(angle = -20, size = 15, vjust = 0)) +
+  xlab("Dział") + ylab('') 
+
 
 ggplot(data = data) +
   geom_mosaic(aes(x = product(W1), fill = D)) +
   theme_mosaic()
 
-ggplot(data = data) +
-  geom_mosaic(aes(x = product(S), fill = P)) +
-  theme_mosaic()
+ggplot(data = my_data) +
+  geom_mosaic(aes(x = product(S,P), fill = S),offset = 0.02) +
+  theme_mosaic() +
+  #scale_y_continuous("Kierownik" , breaks = c(0.5,0.95), labels = c('Nie','Tak'))+
+  scale_x_productlist(labels = c("Kobieta","Mężczyzna")) +
+  scale_fill_manual(values=c(yes,no),
+                    name='Czy jest \n kierownikiem?',
+                    labels = c('Nie','Tak')) +
+  theme(axis.text.y=element_blank(),text = element_text(size = 15), axis.text.x = element_text(size = 15),
+        axis.ticks.y = element_blank()) +
+  ylab('') + xlab('')
+  
 
 ggplot(data = data) +
-  geom_mosaic(aes(x = product(W1), fill = P), offset = 0.05) +
-  theme_mosaic()
+  geom_mosaic(aes(x = product(P,W1), fill = P), offset = 0.02) +
+  theme_mosaic() +
+  scale_fill_manual(values=c(woman, man), 
+                    name='Płeć') +
+  xlab("Ocena") + ylab('Płeć')
 
 mosaicplot(P~W1, data = data)
   
 
 
+#### Zad 5 #####
 
 
+library(stats)
 
+#### proste niezależne (losowanie ze zwracaniem)
 
+wylosowanie <- sample(nrow(data),size = 0.1*nrow(data), TRUE)
 
+wylosowanie
+#### proste zależne (losowanie bez zwracaniem)
+
+NIE_wylosowanie <- sample(nrow(data),size = 0.1*nrow(data), FALSE)
+
+### Zad 6 #####
+
+library(likert)
+ 
+likert(my_data['A1'])
+
+summary(likert(my_data['A1']))
+
+likert.density.plot(likert(my_data['A1']))
+
+likert.bar.plot(likert(my_data['A1']))
+
+#### Zad 7
